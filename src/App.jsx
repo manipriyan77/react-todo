9d14da976e225e9dfb5069d6d47dff3d158d90b4
manipriyan77 <manipriyangopalan@gmail.com>
2023-11-10 22:18:48 +0530

    intialized the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from './components/Input/Input';
+
+const App = () => {
+  return (
+    <div>
+      <h1>todos</h1>
+      <Input />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/store/todoSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+  name: 'Todo',
+  reducers: { addTodo(state, action) {} },
+});
+
+export default todoSlice;
